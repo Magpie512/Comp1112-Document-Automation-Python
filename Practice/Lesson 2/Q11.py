25c680ee9f11dd428f402ab1465b93af4a1565a3
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-01-26 13:45:55 -0500

    Implement number input validation to ensure user inputs a number greater than 5

---
+++ Practice/Lesson 2/Q11.py
@@ -0,0 +1,7 @@
+number = input("Enter a number: ")
+
+while not number > 5:
+    print("The number is not greater than 5.")
+    number = input("Enter a number: ")
+
+print("Thank you! The number is greater than 5.")
