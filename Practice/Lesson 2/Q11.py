0d497b71c86fafdd1c4dfbb329c9eef78247cd56
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-01-26 14:05:42 -0500

    Refactor input handling to ensure number input is consistently validated as an integer

--- Practice/Lesson 2/Q11.py
+++ Practice/Lesson 2/Q11.py
@@ -1,7 +1,9 @@
-number = input("Enter a number: ")
+print("Welcome! Please enter a number greater than 5.")
+
+number = int(input("Enter a number: "))
 
 while not number > 5:
     print("The number is not greater than 5.")
-    number = input("Enter a number: ")
+    number = int(input("Enter a number: "))
 
 print("Thank you! The number is greater than 5.")
