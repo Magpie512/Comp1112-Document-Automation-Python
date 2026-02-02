bdc1cff1b5dd1081925f476fcf032a7cb8a33ad4
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-01 19:51:42 -0500

    bep

---
+++ FINAL/Final.py
@@ -0,0 +1,14 @@
+"""
+Name: Mars Briggs
+Student ID: 200561234
+Date: 2026-02-01    
+Description:
+"""
+
+""" Things ive learned so far is string manipulation, conditional statements, and functions in Python.
+Operators, variables, and input/output handling are also some of the basics I have grasped.
+Lists and loops are other concepts I have learned to use effectively in my code.
+
+I think the task I want to make is 
+I require comments above every block of code to explain its purpose.
+"""
