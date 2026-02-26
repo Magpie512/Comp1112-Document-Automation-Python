5de34de9602446542017ba16468f67418aedcf33
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-02-26 15:50:24 -0500

    Add Modulo.py script with arithmetic operations and print statements

---
+++ School Work/Midterm/Practice/Modulo.py
@@ -0,0 +1,22 @@
+testOne = (17%3)-(5//3)
+
+testTwo = (3//5) - (14%5)
+
+testThree = (25 % 4) + (9 // 2)
+
+testFour = (7 * 3) - (28 % 6)
+
+testFive = (50 // 7) + (50 % 7)
+
+testSix = (18 % 5) * (12 // 7)
+
+testSeven = (4 + 9) % 5
+
+
+print("The answer to testOne is:", testOne,
+      "\nThe answer to testTwo is:", testTwo,
+      "\nThe answer to testThree is:", testThree,
+      "\nThe answer to testFour is:", testFour,
+      "\nThe answer to testFive is:", testFive,
+      "\nThe answer to testSix is:", testSix,
+      "\nThe answer to testSeven is:", testSeven)
