0a6d1006f84b1861c9c9f09594336d5adae4cb59
Mars Briggs <Marsbriggs512@gmail.com>
2026-02-27 13:52:33 -0500

    bruh

---
+++ School Work/Midterm/Practice/forwhile3.py
@@ -0,0 +1,16 @@
+x = 0
+y = 5
+
+for i in range(3, 9, 2):
+    x += 1
+    for j in range(y, 0, -1):
+        if j % 2 == 0:
+            continue
+        x += j
+        print(x, end=" ")
+        if x > 15:
+            break
+    y -= 1
+    if x >= 20:
+        break
+    print("|", end=" ")
