1c852f5319d18297206354bf7587828e87453ceb
Mars Briggs <Marsbriggs512@gmail.com>
2026-02-27 01:43:29 -0500

    Refactor forwhile2.py to improve output clarity and update Written.py to return the new list instead of printing it

--- School Work/Midterm/Practice/forwhile2.py
+++ School Work/Midterm/Practice/forwhile2.py
@@ -3,9 +3,16 @@ b = 2
 y = 0
 
 for x in range(1,5):
-    if x % 2 == 0:
+    if x % 2 == 0: 
         y = (a*y) + (b*x)
     else:
         y = y + (b*x)
+    
+    print(f"After iteration {x}:")
+    print(f"a = {a}")
+    print(f"b = {b}")
+    print(f"x = {x}")
+    print(f"y = {y}")
 
 print(y)
+
