7727aba4a0bdab4500ed0f71d0fc1021b4897361
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-03-17 15:34:21 -0400

    a
    a

---
+++ School Work/Lesson 9/openpyxl/aaa.py
@@ -0,0 +1,82 @@
+"""
+Mars Briggs
+03-17-26
+200561234
+Populating four spreadsheets with the same Fname and Lname, Student number
+Seed each spreadsheet (2, 3, and 4) with a few unique names for testing
+
+Write Eligible graduates to new spreadsheet
+"""
+
+# Imports
+import openpyxl
+
+applyToGraduate = openpyxl.load_workbook("C:\\openpyxl\\applyToGraduate.xlsx")
+bookStoreOwed = openpyxl.load_workbook("C:\\openpyxl\\bookStoreOwed.xlsx")
+libraryOwed = openpyxl.load_workbook("C:\\openpyxl\\libraryOwed.xlsx")
+parkingOwed = openpyxl.load_workbook("C:\\openpyxl\\parkingOwed.xlsx")
+
+# list declares
+eligibleList = []
+mainList = []
+bookList =[]
+libraryList = []
+parkingList = []
+
+
+wb = openpyxl.Workbook()
+output = wb.active
+
+# TEST for cycle
+# for col in columnList:
+#    for row in range(3, 20):
+#        cellName = col + str(row)
+#        value = application[cellName].value
+#        print("Reading cell:", cellName, "Value:", value)
+
+sheet = applyToGraduate.active
+for r in range (2,20):
+    idValue = sheet.cell(row=r,column=3).value
+    if (idValue == None):
+        break
+    mainList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
+
+sheet = bookStoreOwed.active
+for r in range (2,20):
+    idValue = sheet.cell(row=r,column=3).value
+    if (idValue == None):
+        break
+    bookList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
+
+sheet = libraryOwed.active
+for r in range (2,20):
+    idValue = sheet.cell(row=r,column=3).value
+    if (idValue == None):
+        break
+    libraryList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
+
+parking = parkingOwed.active
+for r in range (2,20):
+    idValue = sheet.cell(row=r,column=3).value
+    if (idValue == None):
+        break
+    parkingList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
+
+# Comparison
+
+#print(mainList)
+
+for i in range(len(mainList)):
+    if mainList[i][3] == "0":
+        eligibleList.append(mainList[i])
+
+print(eligibleList)
+
+for row in eligibleList:
+    output.append(row)
+newFile = "Eligible.xlsx"
+wb.save(newFile)
+
+#print(bookList)
+#print(libraryList)
+#print(parkingList)
