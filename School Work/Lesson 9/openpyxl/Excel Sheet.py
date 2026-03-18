b93d7aefea2e7abf89986e1a3a817dec660bf6ab
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-03-18 13:07:43 -0400

    a

--- School Work/Lesson 9/openpyxl/Excel Sheet.py
+++ School Work/Lesson 9/openpyxl/Excel Sheet.py
@@ -1,82 +1,47 @@
-"""
-Mars Briggs
-03-17-26
-200561234
-Populating four spreadsheets with the same Fname and Lname, Student number
-Seed each spreadsheet (2, 3, and 4) with a few unique names for testing
-
-Write Eligible graduates to new spreadsheet
-"""
-
-# Imports
 import openpyxl
+
+ableToGraduate = []
+noOutstandingfeesLibrary = []
+noOutstandingfeesBookstore = []
+noOutstandingfeesParking = []
 
 applyToGraduate = openpyxl.load_workbook("C:\\openpyxl\\applyToGraduate.xlsx")
 bookStoreOwed = openpyxl.load_workbook("C:\\openpyxl\\bookStoreOwed.xlsx")
 libraryOwed = openpyxl.load_workbook("C:\\openpyxl\\libraryOwed.xlsx")
 parkingOwed = openpyxl.load_workbook("C:\\openpyxl\\parkingOwed.xlsx")
 
-# list declares
-eligibleList = []
-mainList = []
-bookList =[]
-libraryList = []
-parkingList = []
-
-
 wb = openpyxl.Workbook()
 output = wb.active
 
-# TEST for cycle
-# for col in columnList:
-#    for row in range(3, 20):
-#        cellName = col + str(row)
-#        value = application[cellName].value
-#        print("Reading cell:", cellName, "Value:", value)
-
-sheet = applyToGraduate.active
-for r in range (2,20):
-    idValue = sheet.cell(row=r,column=3).value
-    if (idValue == None):
-        break
-    mainList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
-
+# Bookstore
 sheet = bookStoreOwed.active
-for r in range (2,20):
-    idValue = sheet.cell(row=r,column=3).value
-    if (idValue == None):
-        break
-    bookList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
+for i in range (2,20):
+    owed = sheet.cell(row=i,column=5).value
+    if ( owed == 0): # acessing raw document
+        noOutstandingfeesBookstore.append([str(sheet.cell(row=i,column=2).value),str(sheet.cell(row=i,column=3).value),str(sheet.cell(row=i,column=4).value),str(sheet.cell(row=i,column=5).value)])
 
+# Library
 sheet = libraryOwed.active
-for r in range (2,20):
-    idValue = sheet.cell(row=r,column=3).value
-    if (idValue == None):
-        break
-    libraryList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
+for i in range (2,20):
+    owed = sheet.cell(row=i,column=5).value
+    if ( owed == 0): # acessing raw document
+        noOutstandingfeesLibrary.append([str(sheet.cell(row=i,column=2).value),str(sheet.cell(row=i,column=3).value),str(sheet.cell(row=i,column=4).value),str(sheet.cell(row=i,column=5).value)])
 
-parking = parkingOwed.active
-for r in range (2,20):
-    idValue = sheet.cell(row=r,column=3).value
-    if (idValue == None):
-        break
-    parkingList.append([str(sheet.cell(row=r,column=2).value),str(sheet.cell(row=r,column=3).value),str(sheet.cell(row=r,column=4).value),str(sheet.cell(row=r,column=5).value)])
+# Parking
+sheet = parkingOwed.active
+for i in range (2,20):
+    owed = sheet.cell(row=i,column=5).value
+    if ( owed == 0): # acessing raw document
+        noOutstandingfeesParking.append([str(sheet.cell(row=i,column=2).value),str(sheet.cell(row=i,column=3).value),str(sheet.cell(row=i,column=4).value),str(sheet.cell(row=i,column=5).value)])
 
-# Comparison
+# Compare to see if student is eligible
+for i in range(len(noOutstandingfeesBookstore)):
+    if noOutstandingfeesBookstore[i][3] == "0" and noOutstandingfeesLibrary[i][3] == "0" and noOutstandingfeesParking[i][3] == "0":
+        ableToGraduate.append([noOutstandingfeesBookstore[i][0],noOutstandingfeesBookstore[i][1],noOutstandingfeesBookstore[i][2],noOutstandingfeesBookstore[i][3]])
 
-#print(mainList)
+print(ableToGraduate)
 
-for i in range(len(mainList)):
-    if mainList[i][3] == "0":
-        eligibleList.append(mainList[i])
-
-print(eligibleList)
-
-for row in eligibleList:
+for row in ableToGraduate:
     output.append(row)
 newFile = "Eligible.xlsx"
 wb.save(newFile)
-
-#print(bookList)
-#print(libraryList)
-#print(parkingList)
