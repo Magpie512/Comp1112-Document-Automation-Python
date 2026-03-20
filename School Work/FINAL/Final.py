b3ce4cb43d2dccde8d2c431b33a470d220c03f55
Mars Briggs <Marsbriggs512@gmail.com>
2026-03-20 09:47:52 -0400

    a

--- School Work/FINAL/Final.py
+++ School Work/FINAL/Final.py
@@ -10,7 +10,9 @@ Things ive learned so far is string manipulation, conditional statements, and fu
 Operators, variables, and input/output handling are also some of the basics I have grasped.
 Lists and loops are other concepts I have learned to use effectively in my code.
 read writing files and handling exceptions are also part of my skill set now.
-Regex and object-oriented programming are more advanced topics I have started to explore.
+Regex 
+Excel scraping
+and webscraping
 
 I think the task I want to make is a resume builder. 
 I require comments above every block of code to explain its purpose.
---
+++ School Work/FINAL/Tool.py
@@ -0,0 +1,11 @@
+"""
+Mars Briggs
+200561234
+2026-03-20
+Game Price Point comparisson
+"""
+# For Web scraping
+import urllib.request
+from bs4 import BeautifulSoup
+
+def inputSites:
