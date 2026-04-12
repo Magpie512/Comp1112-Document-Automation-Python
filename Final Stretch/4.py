4aac7d4e8d62fcc60b2fd1ea6cfbf2d78bb71744
Mars Briggs <Marsbriggs512@gmail.com>
2026-04-11 22:24:15 -0400

    a

---
+++ Final Stretch/4.py
@@ -0,0 +1,11 @@
+resultList = []
+def mysteryFunc(n):
+    if n <= 1:
+        return 1
+    
+resultList.append(n)
+return n + mysteryFunc(n - 2)
+answer = mysteryFunc(7)
+print(str(answer))
+print(str(resultList))
+print(str(len(resultList)))
