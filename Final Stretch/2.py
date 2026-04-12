936560b539cf073c6bdadce55ced2f79e2e1b538
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-04-11 23:10:21 -0400

    a
    a

--- Final Stretch/2.py
+++ Final Stretch/2.py
@@ -4,11 +4,11 @@ for i in range(3): # occurs 3 times
     for j in range(2, 5): # occurs 3 times
         for k in range(1, 4): # occurs 3 times
             counter = counter + 1
-            if (i + j + k) % 3 == 0:
-                continue
-            if counter > 12:
-                break
-        result = result + str(i * j * k)
+            if (i + j + k) % 3 == 0: # if i + j + k is divisible by 3
+                continue # skip the rest of the loop
+            if counter > 12: # if counter is greater than 12
+                break # break the loop
+        result = result + str(i * j * k) # add if any 0 then 0
         if counter > 12:
             break
 
