355c90a60657639c975e4bd0b094187f4eb94244
Magpie512 <224426406+Magpie512@users.noreply.github.com>
2026-04-14 10:13:03 -0400

    a
    a

---
+++ Final Stretch/epoch.py
@@ -0,0 +1,5 @@
+import time
+
+timeStart = time.time()
+timeStart = time.ctime(timeStart)
+print(timeStart)
--- Final Stretch/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    password = str(alphabet[1])+str(alphabet[2])+str(alphabet[3])+str(alphabet[4])+str(alphabet[5])+str(alphabet[6])+str(alphabet[7])+str(alphabet[8])+str(alphabet[9])
-                                        if password == phrase:
-                                            cracked = True
-                                            print(password)
-                                            break
